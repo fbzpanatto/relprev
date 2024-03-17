811ecac90dcc4ae3c16bc47ef7ca4cd916052bb5
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-03-17 02:28:25 -0300

    Custom Drop Down

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, input, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ClickOutsideDirective } from '../../directives/clickOutside.directive';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [
-    CommonModule, ReactiveFormsModule
+    CommonModule, ReactiveFormsModule, ClickOutsideDirective
   ],
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss', '../../../styles/general.scss'],
@@ -15,6 +16,7 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 export class FormComponent implements OnInit {
 
   typeOfMedia = input('')
+  showSelectOptions = signal(false)
   #currentFileName?: string
 
   form = this.fb.group({
@@ -63,11 +65,37 @@ export class FormComponent implements OnInit {
     console.log('resetando form...')
   }
 
+  onSelectClick() {
+    this.showSelectOptions.update(curr => curr = !curr)
+  }
+
+  onOutsideClick() {
+    this.showSelectOptions.update(curr => curr = false)
+  }
+
+  onClickOption(option: { id: number, value: string }) {
+
+    this.showSelectOptions.update(curr => curr = !curr)
+    this.form.patchValue({ reporterActivityArea: option.value })
+  }
+
   setFile($event: Event, el: HTMLInputElement) {
     $event.preventDefault()
     el.click()
     const file = el.files?.item(0)
     this.currentFileName = file?.name
+  }
+
+  get options() {
+    return [
+      { id: 1, value: 'Option 1' },
+      { id: 2, value: 'Option 2' },
+      { id: 3, value: 'Option 3' },
+      { id: 4, value: 'Option 4' },
+      { id: 5, value: 'Option 5' },
+      { id: 6, value: 'Option 6' },
+      { id: 7, value: 'Option 7' }
+    ]
   }
 
   get currentFileName() { return this.#currentFileName }
